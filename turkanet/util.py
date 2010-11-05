28ad5bdd79b66c364bac095002af929eff7562b7
Tim Fletcher <mail@tfletcher.com>
2010-11-05 15:44:02 +0000

    Add Cycle helper class.

--- turkanet/util.py
+++ turkanet/util.py
@@ -1,6 +1,22 @@
 import os, hashlib
 
 
+class Cycle(object):
+  def __init__(self, values):
+    self.values = values
+
+    self.length = len(values)
+
+    self.index = 0
+
+  def next(self):
+    value = self.values[self.index]
+
+    self.index = (self.index + 1) % self.length
+
+    return value
+
+
 def nonce():
   sha = hashlib.sha1()
 
